833e7e6afe51fd3a1b8e411256ff7ca787c390df
hyeonnkang <hyeonkang.dev@gmail.com>
2025-03-09 18:10:40 +0900

    refactor : OCP - change game level

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,9 +1,14 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.Begginer;
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+
 public class GameApplication {
 
     public static void main(String[] args) {
-        Minesweeper minesweeper = new Minesweeper();
+        GameLevel gameLevel = new Begginer();
+        
+        Minesweeper minesweeper = new Minesweeper(gameLevel);
         minesweeper.run();
     }
 
