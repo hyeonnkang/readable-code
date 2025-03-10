b026c19ad6b7f738420ec9c4a1d0846ea7b55986
hyeonnkang <hyeonkang.dev@gmail.com>
2025-03-10 16:30:54 +0900

    refactor : extract domain GameConfig

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,5 +1,6 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.config.GameConfig;
 import cleancode.minesweeper.tobe.gamelevel.Begginer;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
@@ -10,11 +11,12 @@ import cleancode.minesweeper.tobe.io.OutputHandler;
 public class GameApplication {
 
     public static void main(String[] args) {
-        GameLevel gameLevel = new Begginer();
-        InputHandler inputHandler = new ConsoleInputHandler();
-        OutputHandler outputHandler = new ConsoleOutputHandler();
+        GameConfig gameConfig = new GameConfig(
+                new Begginer(),
+                new ConsoleInputHandler(),
+                new ConsoleOutputHandler());
 
-        Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
+        Minesweeper minesweeper = new Minesweeper(gameConfig);
         minesweeper.initialize();
         minesweeper.run();
     }
